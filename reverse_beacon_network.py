3cc29d6bf644416682f6b4784c69fb7e2673f74f
David Mittelman <1123783+ctartist621@users.noreply.github.com>
2022-03-21 19:12:03 -0700

    Reverse_beacon_network download script

---
+++ reverse_beacon_network.py
@@ -0,0 +1,26 @@
+import os
+import zipfile
+
+import requests
+
+for file_name in range(20220101, 20220201):
+    print(f"Retrieving {file_name}")
+    resp = requests.get(f"http://reversebeacon.net/raw_data/dl.php?f={file_name}")
+
+    zip_file = f"/Users/dave/code/contact_genie/data/{file_name}.zip"
+    f = open(zip_file, mode="wb")
+    f.write(resp.content)
+    f.close()
+
+    try:
+        with zipfile.ZipFile(zip_file) as z:
+            with open(
+                f"/Users/dave/code/contact_genie/data/reverse_beacon_network/{file_name}.csv", mode="w"
+            ) as f:
+                f.write(z.read(f"{file_name}.csv").decode("ascii"))
+                print("Extracted", f"{file_name}.csv")
+
+    except:
+        print("Invalid file")
+
+    os.remove(zip_file)
